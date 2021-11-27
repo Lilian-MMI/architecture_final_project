e88aa167a242be88462cec1f7c5531ba4daefd6b
AurelienV49 <aurelien.vaillant49@ynov.com>
2021-11-27 19:49:20 +0100

    AVA : Ajout projet API Gateway. Création des routes.

---
+++ API Gateway/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");// pour les log
+const cors = require("cors"); // Pour accèpter uniquement les Requêtes de telle URL
+const PORT_API = 3001;
+const amqp = require("amqplib/callback_api");
+
+/* ONLY ACCEPT Request*/
+let corsOptions = {
+    origin: ["http://localhost:3000"],
+    credentials: true,
+};
+
+/* SETUP APP MIDDLEWARE */
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
+
+/* SETUP RENDER */
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+/* IMPORT ROUTES */
+require("./routes/api.routes.user")(app);
+require("./routes/api.routes.game")(app);
+require("./routes/api.routes.score")(app);
+
+// Si aucune route n'a été trouvée
+app.get('/.*/', function (req, res) {
+
+    res.writeHead(404);
+    resend("Erreur 404 : URL " + req.url + " inconnue !")
+
+});
+
+/* APP LISTEN ON */
+app.listen(PORT_API, () => {
+    console.log("\x1b[33m%s\x1b[0m", `Server API Gateway is running on port ${PORT_API} ✔`);
+});
