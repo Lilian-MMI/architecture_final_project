9126c17f2d0572eba65924abc32ecf5df2db01e7
AurelienV49 <aurelien.vaillant49@ynov.com>
2021-11-30 22:01:45 +0100

    Update 4 API Gateway

--- API Gateway/index.js
+++ API Gateway/index.js
@@ -1,19 +1,14 @@
 const express = require("express");
-<<<<<<< HEAD
 const morgan = require("morgan");
 const cors = require("cors");
-=======
-const morgan = require("morgan"); // pour les log
-const cors = require("cors"); // Pour accèpter uniquement les Requêtes de telle URL
->>>>>>> 48392efff71eaf834a202fc735a6949c4547a430
 const PORT_API = 3001;
 const cookieParser = require("cookie-parser");
 const amqp = require("amqplib/callback_api");
 
 /* ONLY ACCEPT Request*/
 let corsOptions = {
-  origin: ["http://localhost:3000"],
-  credentials: true,
+    origin: ["http://localhost:3000"],
+    credentials: true,
 };
 
 /* SETUP APP MIDDLEWARE */
@@ -30,28 +25,14 @@ require("./routes/api.routes.game")(app);
 require("./routes/api.routes.score")(app);
 
 // Si aucune route n'a été trouvée
-<<<<<<< HEAD
 app.get('/.*/', function(req, res) {
 
     res.writeHead(404);
     res.end("Erreur 404 : URL " + req.url + " inconnue !")
 
-=======
-app.get("/.*/", function (req, res) {
-  res.writeHead(404);
-  resend("Erreur 404 : URL " + req.url + " inconnue !");
->>>>>>> 48392efff71eaf834a202fc735a6949c4547a430
 });
 
 /* APP LISTEN ON */
 app.listen(PORT_API, () => {
-<<<<<<< HEAD
     console.log("\x1b[33m%s\x1b[0m", `Server API Gateway is running on port ${PORT_API} ✔`);
 });
-=======
-  console.log(
-    "\x1b[33m%s\x1b[0m",
-    `Server API Gateway is running on port ${PORT_API} ✔`
-  );
-});
->>>>>>> 48392efff71eaf834a202fc735a6949c4547a430
